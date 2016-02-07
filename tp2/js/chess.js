40e07dce791571d27f9f0b45c813c26d1febf3ee
BenjaminDeregnaucourt <benji94111995@gmail.com>
2016-02-07 19:08:27 +0100

    affichage pb generation piece

--- tp2/js/chess.js
+++ tp2/js/chess.js
@@ -1,21 +1,3 @@
-<html>
-  <head>
-  <script type='text/javascript'>
-// On tente de charger l'image au plus tôt !
-var chessSymbols = new Image();
-chessSymbols.src = 'chess.png';
-chessSymbols.onload = function() {
-    console.info("Chess symbols loaded !");
-    
-    // dessine la grille, une fois l'image chargée
-    drawGrid(0,0, canvas.width, canvas.height, 8, 8); 
-};
-    </script>
-  </head>
-  <body>
-    <h1>Deep blue version JS</h1>
-    <center><canvas id='screen' width='400' height='400' >JavaScript est nécessaire ...</canvas></center>
-    <script type='text/javascript'>
 
 var canvas = document.getElementById("screen");
 var gfx    = canvas.getContext("2d");
@@ -42,13 +24,17 @@ Piece.prototype.orientation = function() {
 var Pawn = function(color, line, column) {
     Piece.prototype.constructor.call(this, 'Pawn', color, line, column);
     this.pieceId = [[0, 5], [480, 5]];
+	this.move= [[line+1,column],[line-1,column];
+	this.debut= [[line+1,column],[line+2,column],[line-2,column],[line-1,column]];
+	this.attack=[line+1,column-1],[line+1,column+1],[line-1,colum-1],[line-1,colum+1]];
 }
 Pawn.prototype = new Piece();
 
 var Rook = function(color,line,column){
     Piece.prototype.constructor.call(this,'Rook',color,line,column);
     this.pieceId =[[225, 5],[705, 5]];
-}
+	
+	}
 Rook.prototype = new Piece();
 
 var Bishop = function(color,line,column){
@@ -71,17 +57,29 @@ var King = function(color,line,column){
 King.prototype = new Piece();
 
 var Queen =function(color,line,column){
-    Piece.prototype.constructor.call(this.'Queen",color,line,column);
-    this.pieceId =[[330,5],[810,5];
+    Piece.prototype.constructor.call(this,'Queen',color,line,column);
+    this.pieceId =[[330,5],[810,5]];
 }
 Queen.prototype = new Piece();
 
 //var createBoard = function(nbLine, nbColumn) // avec des pieces vide
   
 //var isEmpty = function(lig, col)
-//var put = function(lig, col, piece)
-
-//var board = createBoard(8, 8);
+	
+var put = function(lig, col, piece){
+	var chessSymbols = new Image();
+	chessSymbols.src = 'chess.png';
+	chessSymbols.onload = function() {
+		if(piece.color==='white'){
+		gfx.drawImage(chessSymbols,piece.pieceId[0][0],piece.pieceId[0][1],75,75,75*lig,75*col,75,75);
+		
+		}
+		if(piece.color==='black'){
+			gfx.drawImage(chessSymbol,piece.pieceId[1][0],piece.pieceId[1][1],75,75,75*lig,75*col,75,75);
+		}
+	}
+}
+//var board = createBoard(8, 8)
 
 //var initBoard = function() // avec les pièces du jeu
 
@@ -93,17 +91,17 @@ var convertCoordinates = function(ligPixel, colPixel) {
 
 var highlightedCells = [];
 
-var drawCell = function(coord, color){
+/*var drawCell = function(coord, color){
   gfx.fillStyle = color;
   gfx.fillRect(coord.lig,coord.col,canvas.width/8,canvas.height/8);
-}
+}*/
     
 //var highlight = function(on)
     
 canvas.addEventListener("mousedown", mouseClicked, false);
 //var mouseClicked = function(event) { // Pourquoi cela ne fonctionne pas avec var ?!
 function mouseClicked(event) {
-/*    var ligCoord = event.pageY - canvas.offsetTop;
+	var ligCoord = event.pageY - canvas.offsetTop;
     var colCoord = event.pageX - canvas.offsetLeft;
     var coord    = convertCoordinates(ligCoord, colCoord);
     console.info(coord);
@@ -119,21 +117,46 @@ function mouseClicked(event) {
     } else {
         // TODO: if empty and highlighted, move the piece !
         // doMove();
-    }*/
+    }
 }
 
 //initBoard();
 
 var drawGrid = function(x, y, width, height, nbLig, nbCol) {
   gfx.strokeStyle ='red';
-  gfx.StrokeRect(0,0,width,height);
-  for(var i = 0; i<nblig;i++){
-    for(var j = 0;j<nbCol;j++){
-      gfx.strokeStyle= 'red';
-      gfx.strokeRect(j*width/nbCol,i*height*nbLig,width/nbCol,height/nbLig);
-    }
+  gfx.strokeRect(x,y,width,height);
+  var j=0;
+  var i=0;
+  var couleur =0;
+  while(i<nbLig){
+    while(j<nbCol){
+      gfx.strokeStyle= 'black';
+      gfx.strokeRect(x+(j*width/nbCol),y+(i*height/nbLig),(width/nbCol),(height/nbLig));
+	  if (couleur ===0){
+		  couleur=1;
+		  gfx.fillStyle='white';
+		  gfx.fillRect(x+1+(j*width/nbCol),y+1+(i*height/nbLig),(width/nbCol)-2,(height/nbLig)-2);
+	  }else if(couleur ===1){
+		  couleur=0;
+		  gfx.fillStyle='black';
+		  gfx.fillRect(x+1+(j*width/nbCol),y+1+(i*height/nbLig),(width/nbCol)-2,(height/nbLig)-2);
+	  }
+	  
+	  j++;
+	  //console.log(j);
+	}
+	i++;
+	 if (couleur ===0){
+		  couleur=1;
+	 }
+	 else if(couleur ===1){
+		  couleur=0;
+	 }
+	j=0;
+	//console.log(i);
   }
+ // Pion rekt =new Pion('white',4,4);
+  //put(rekt.lig,rekt.col,rekt);
+  //put()
 }
-    </script>
-  </body>
-</html>
+
